305e47fb492da11cb3d2adb548888e95522a571c
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2026-04-19 21:12:19 +0100

    refactor: improve token handling in protect middleware

--- middlewares/protect.js
+++ middlewares/protect.js
@@ -1,37 +1,39 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
-const protect = async (req, res, next)=>{
+const protect = async (req, res, next) => {
     try {
-        
-        const token = req.cookies.refreshToken
-    
-        if(!token) return res.status(401).json({
-            status: "Error",
-            message: "User not authorized"
-        })
-    
-        const verifyUser = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
-        const user = await User.findOne({_id: verifyUser.id}).select("-password")
-        soc
-    
-        if(!user){
+        let token = req.cookies && req.cookies.refreshToken;
+
+        if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+
+        if (!token) {
+            return res.status(401).json({
+                status: "Error",
+                message: "User not authorized"
+            });
+        }
+
+        const verifyUser = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+        const user = await User.findOne({ _id: verifyUser.id }).select("-password");
+
+        if (!user) {
             return res.status(404).json({
                 status: "Error",
                 message: "User not found"
-            })
+            });
         }
-    
-        req.user = user
-    
-    next()
+
+        req.user = user;
+        next();
     } catch (error) {
         return res.status(401).json({
             status: "Error",
             message: "Invalid or expired token"
-        })    
+        });
     }
-
-}
+};
 
 module.exports = protect
